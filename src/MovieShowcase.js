fc85ef48e737eea363cd304d8d399fdb82595a8b
Joseph Tarrazi <joseftaerrazi@gmail.com>
2021-01-06 08:05:28 +0000

    Automatically backed up by Learn

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react';
 import MovieCard from './card-components/MovieCard.js';
 import movieData from './data.js';
+<<<<<<< HEAD
+=======
+
+
+
+class List extends React.Component{
+  render(){
+    return <li>{this.props.content}</li>;
+  }
+}
+
+>>>>>>> 13a634afb48810420cf360cd2562775109bcd942
 
 export default class MovieShowcase extends Component {
 	generateMovieCards = () => {
@@ -13,7 +25,22 @@ export default class MovieShowcase extends Component {
 		    />)
 	};
 
+<<<<<<< HEAD
 	render() {
 		return <div id="movie-showcase">{this.generateMovieCards()}</div>;
 	}
+=======
+  generateMovieCards = () => {
+    // map over your movieData array and return an array of the correct JSX
+    return movieData.map(item=> <List content={item}/>)
+  }
+
+  render() {
+    return (
+      <div id="movie-showcase">
+        {this.generateMovieCards()}
+      </div>
+    )
+  }
+>>>>>>> 13a634afb48810420cf360cd2562775109bcd942
 }
--- src/card-components/CardFront.js
+++ src/card-components/CardFront.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React, { Component } from 'react';
 
 export default class CardFront extends Component {
@@ -10,3 +11,16 @@ export default class CardFront extends Component {
 		);
 	}
 }
+=======
+import React, { Component } from 'react';
+
+export default class CardFront extends Component {
+
+  render() {
+    return (
+      <div className="card-front" style={{backgroundImage: `url(${'prop'})`}}>
+      </div>
+    )
+  }
+}
+>>>>>>> 13a634afb48810420cf360cd2562775109bcd942
